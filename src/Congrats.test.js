b98245e7e1711a0f9ee38484391fcd38ad459bdf
BIJAY SINGH <bijay.prakash7@yahoo.in>
2020-04-07 22:29:24 +0530

    Added propTypes and Prop types check test

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -2,17 +2,20 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Congrats from "./Congrats.component";
-import { findById } from "./test/testUtils";
+import { findById, checkProps } from "./test/testUtils";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("Jotto", () => {
+  const defaultProps = { success: false };
+
   const setup = (props = {}) => {
-    return shallow(<Congrats {...props} />);
+    const setupProps = { ...defaultProps, ...props };
+    return shallow(<Congrats {...setupProps} />);
   };
 
   it("should renders without error", () => {
-    const wrapper = setup();
+    const wrapper = setup({ success: false });
     const component = findById(wrapper, "component-congrats");
     expect(component.length).toBe(1);
   });
@@ -27,5 +30,10 @@ describe("Jotto", () => {
     const wrapper = setup({ success: true });
     const message = findById(wrapper, "congrats-message");
     expect(message.text().length).not.toBe(0);
+  });
+
+  it("should not throw warning with expected prop type", () => {
+    const expectedProps = { success: false };
+    checkProps(Congrats, expectedProps);
   });
 });
--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -1,3 +1,15 @@
+import checkPropTypes from "check-prop-types";
+
 export const findById = (wrapper, val) => {
   return wrapper.find(`#${val}`);
+};
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    "props",
+    component.name
+  );
+  expect(propError).toBeUndefined();
 };
