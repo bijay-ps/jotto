9618ee4a10865c82d3495fccfc9216dfe936bc2b
BIJAY SINGH <bijay.prakash7@yahoo.in>
2020-04-16 22:03:43 +0530

    Added propTypes to Input component

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { findById } from "./test/testUtils";
+import { checkProps, findById } from "./test/testUtils";
 import Input from "./Input.component";
 
 describe("Input component", () => {
-  const setUp = () => {
-    return shallow(<Input />);
+  const setUp = (secretWord = "party") => {
+    return shallow(<Input secretWord={secretWord} />);
   };
 
   it("should render without any error", () => {
     const wrapper = setUp();
     const component = findById(wrapper, "component-Input");
     expect(component.length).toBe(1);
+  });
+
+  it("should not throw warning for expected prop types", () => {
+    checkProps(Input, { secretWord: "party" });
   });
 });
