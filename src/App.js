80b206d670fd45e5cc397a19af5840bda45c0a91
BIJAY SINGH <bijay.prakash7@yahoo.in>
2020-04-17 22:00:44 +0530

    Added spinner and required test cases

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
 import React from "react";
 import "./App.css";
-import hookActions from './actions/hookActions';
+import hookActions from "./actions/hookActions";
 import Congrats from "./Congrats.component";
 import GuessedWords from "./GuessedWords.component";
+import Input from "./Input";
 
+/**
+ *
+ * @param {Object} state
+ * @param {Object} action
+ * @returns {{secretWord: any}}
+ */
 function reducer(state, action) {
-  switch(action.type) {
-    case 'setSecretWord':
+  switch (action.type) {
+    case "setSecretWord":
       return {
         ...state,
         secretWord: action.payload
-      }
+      };
     default:
-      throw new Error(`Invalid action type: ${action.type}`)
+      throw new Error(`Invalid action type: ${action.type}`);
   }
 }
 
 const App = () => {
-  const [state, dispatch] = React.useReducer(
-      reducer,
-      { secretWord: null }
-  )
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
 
-  const setSecretWord = (secretWord) => dispatch({ type: 'setSecretWord', payload: secretWord })
-
+  const setSecretWord = secretWord =>
+    dispatch({ type: "setSecretWord", payload: secretWord });
 
   React.useEffect(() => {
-    hookActions.getSecretWord(setSecretWord)
-  }, [])
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
+
+  if (!state.secretWord) {
+    return (
+      <div className="container" id="spinner">
+        <div className="spinner-border" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+        <p>Loading secret word</p>
+      </div>
+    );
+  }
+
   return (
     <div className="container" id="component-App">
       <h1>Jotto</h1>
+      <Input secretWord={state.secretWord} />
       <Congrats success={true} />
       <GuessedWords
         guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
--- src/App.test.js
+++ src/App.test.js
@@ -6,9 +6,11 @@ import hookActions from "./actions/hookActions";
 
 const mockGetSecretWord = jest.fn();
 
-const setUp = () => {
+const setUp = (secretWord = "party") => {
   mockGetSecretWord.mockClear();
   hookActions.getSecretWord = mockGetSecretWord;
+
+  React.useReducer = jest.fn().mockReturnValue([{ secretWord }, jest.fn()]);
 
   // use mount, because useEffect not called on 'shallow'
   // https://github.com/airbnb/enzyme/issue/2806
@@ -33,5 +35,39 @@ describe("getSecretWord calls", () => {
     mockGetSecretWord.mockClear();
     wrapper.setProps();
     expect(mockGetSecretWord).not.toHaveBeenCalled();
+  });
+});
+
+describe("Secret word is not null", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setUp("party");
+  });
+
+  it("should render the App when secret word is not null", () => {
+    const appComponent = findById(wrapper, "component-App");
+    expect(appComponent.exists()).toBe(true);
+  });
+
+  it("should not render the spinner when secret word is not null", () => {
+    const spinnerComponent = findById(wrapper, "spinner");
+    expect(spinnerComponent.exists()).toBe(false);
+  });
+});
+
+describe("Secret word is null", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setUp(null);
+  });
+
+  it("should not render the App when secret word is null", () => {
+    const appComponent = findById(wrapper, "component-App");
+    expect(appComponent.exists()).toBe(false);
+  });
+
+  it("should render the spinner when secret word is null", () => {
+    const spinnerComponent = findById(wrapper, "spinner");
+    expect(spinnerComponent.exists()).toBe(true);
   });
 });
